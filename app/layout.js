0ab1eb7d60e562d3e81348cf9fce303acaf85d75
Krushnal Patel <krushnalpatel11@gmail.com>
2024-03-25 17:30:04 +0530

    added vercel analytics and speed insights

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter, } from 'next/font/google'
 import './globals.css'
 import 'react-toastify/dist/ReactToastify.css';
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +15,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className + 'w-full h-full'}>
+        <Analytics />
+        <SpeedInsights />
         {children}
       </body>
     </html>
