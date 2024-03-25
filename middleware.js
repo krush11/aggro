58c1625982572d7636dbf79df9319562f6716986
Krushnal Patel <krushnalpatel11@gmail.com>
2024-03-25 17:25:09 +0530

    removed env and middleware; fixed build command

--- middleware.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function middleware(req) {
-  const headersList = new Headers(req.headers);
-  headersList.set('x-pathname', req.nextUrl.pathname);
-
-  return NextResponse.next({ request: { headers: headersList } });
-}
